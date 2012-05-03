27d25c1d182968ee7f167b7d4acd33f40479e7bd
Ovid <curtis@weborama.com>
2012-05-03 10:53:20 +0200

    Add tab completion to search types.
    This makes it much easier to handle an individual search and use long,
    descriptive names.

--- js/personalSearch.js
+++ js/personalSearch.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
 
 String.prototype.redirectFormat = function() {
     var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
+    return this.replace(/{(\d+)}/g, function(match, number) {
         return typeof args[number] != 'undefined'
           ? args[number]
           : match;
@@ -39,14 +39,14 @@ String.prototype.redirectFormat = function() {
 
 var personalSearch = function(searchId) {
     var redirects = {
-        c : "http://search.cpan.org/search?query={0}&mode=all",
-        f : "http://www.flickr.com/search/?q={0}&l=comm&ss=2&ct=6&mt=all&w=all&adv=1",
-        g : "http://www.google.com/search?btnG=1&pws=0&q={0}",
-        m : "https://metacpan.org/search?q={0}",
-        n : "http://www.google.com/search?btnG=1&tbm=nws&pws=0&q={0}",
-        p : "https://www.google.com/search?hl=en&q=site%3Apublius-ovidius.livejournal.com+{0}",
-        w : "http://www.wolframalpha.com/input/?i={0}",
-        y : "http://www.youtube.com/results?search_query={0}"
+        cpan         : "http://search.cpan.org/search?query={0}&mode=all",
+        flickrcc     : "http://www.flickr.com/search/?q={0}&l=comm&ss=2&ct=6&mt=all&w=all&adv=1",
+        google       : "http://www.google.com/search?btnG=1&pws=0&q={0}",
+        metacpan     : "https://metacpan.org/search?q={0}",
+        news         : "http://www.google.com/search?btnG=1&tbm=nws&pws=0&q={0}",
+        publius      : "https://www.google.com/search?hl=en&q=site%3Apublius-ovidius.livejournal.com+{0}",
+        wolframalpha : "http://www.wolframalpha.com/input/?i={0}",
+        youtube      : "http://www.youtube.com/results?search_query={0}"
     };
 
     $(searchId).submit(function() {
@@ -66,4 +66,27 @@ var personalSearch = function(searchId) {
             return true;
         }
     });
-};
+    /* http://stackoverflow.com/questions/1837555/ajax-autocomplete-or-autosuggest-with-tab-completion-autofill-similar-to-shell
+     * */
+    var keys = [];
+    for (var key in redirects) keys.push('!' + key);
+    keys.sort();
+    $('#search').autocomplete({
+        source: keys,
+        delay: 0,
+        selectFirst: true,
+        select: function(event, ui) {
+            var TABKEY = 9;
+            this.value = ui.item.value;
+
+            if (event.keyCode == TABKEY) {
+                event.preventDefault();
+                this.value = this.value + " ";
+                $('#search').focus();
+            }
+
+            return false;
+        },
+        autoFocus: true
+    });
+}
